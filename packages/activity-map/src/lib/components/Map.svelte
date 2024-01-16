3fa382844bab6d319267468f26e9caeda1540789
parkerziegler <parkerellisziegler@gmail.com>
2024-01-16 13:59:13 -0800

    feat: Check in Map component for activity map.

--- packages/activity-map/src/lib/components/Map.svelte
+++ packages/activity-map/src/lib/components/Map.svelte
@@ -1,31 +1,26 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import * as d3 from 'd3';
-	import * as topojson from 'topojson-client';
 	import type { FeatureCollection, Feature, Point } from 'geojson';
 	import type { ParcelProperties } from '@land-grab-2-interactives/types';
 
 	import { activity } from '$lib/stores/activity';
 	import { ACTIVITY_CONFIG } from '$lib/utils/activity-config';
-	import { GRIST_COLORS } from '$lib/utils/constants';
+	import { GRIST_COLORS, CANVAS_DIMENSIONS, SPIKE_OPACITY } from '$lib/utils/constants';
+
+	export let scale: d3.ScaleLinear<number, number>;
+	export let us: Feature;
+	export let states: FeatureCollection;
+	export let parcels: FeatureCollection<Point, ParcelProperties>;
+	export let universities: FeatureCollection<Point>;
 
 	let canvas: HTMLCanvasElement;
 
-	const MAX_SPIKE_LENGTH = 100;
-	const CANVAS_DIMENSIONS = {
-		width: 975,
-		height: 700
-	};
-
-	const projection = d3.geoAlbersUsa().scale(1300).translate([487.5, 350]);
+	const projection = d3
+		.geoAlbersUsa()
+		.scale(1300)
+		.translate([CANVAS_DIMENSIONS.width / 2, CANVAS_DIMENSIONS.height / 2]);
 	const spike = (length: number, width = 6) => `M${-width / 2},0L0,${-length}L${width / 2},0`;
-
-	let domain: [number, number] = [0, 0];
-	const range = [0, MAX_SPIKE_LENGTH];
-	let scale = d3.scaleLinear(domain, range);
-
-	let us: Feature;
-	let parcels: Feature<Point, ParcelProperties>[] = [];
 
 	const draw = (
 		parcels: Feature<Point, ParcelProperties>[],
@@ -35,10 +30,27 @@
 		ctx.clearRect(0, 0, CANVAS_DIMENSIONS.width, CANVAS_DIMENSIONS.height);
 
 		const path = d3.geoPath(projection, ctx);
+
 		ctx.beginPath();
 		path(us);
+		ctx.shadowColor = 'rgba(60, 56, 48, 0.3)';
+		ctx.shadowOffsetY = -5;
+		ctx.shadowBlur = 10;
 		ctx.lineWidth = 0.5;
 		ctx.strokeStyle = GRIST_COLORS.EARTH;
+		ctx.stroke();
+		ctx.fillStyle = '#ffffff';
+		ctx.fill();
+
+		ctx.shadowColor = 'rgba(0, 0, 0, 0)';
+		ctx.shadowOffsetX = 0;
+		ctx.shadowOffsetY = 0;
+		ctx.shadowBlur = 0;
+
+		ctx.beginPath();
+		path(states);
+		ctx.lineWidth = 0.25;
+		ctx.strokeStyle = 'rgba(60, 56, 48, 0.3)';
 		ctx.stroke();
 
 		parcels.forEach((parcel) => {
@@ -53,10 +65,51 @@
 			ctx.strokeStyle = ACTIVITY_CONFIG[$activity].color;
 			ctx.stroke(p);
 			ctx.fillStyle = ACTIVITY_CONFIG[$activity].color;
-			ctx.globalAlpha = 0.05;
+			ctx.globalAlpha = SPIKE_OPACITY;
 			ctx.fill(p);
 
 			ctx.restore();
+		});
+
+		const parcelUniversities = new Set(parcels.map((parcel) => parcel.properties.university));
+		parcelUniversities.forEach((parcelUniversity) => {
+			const university = universities.features.find(
+				(university) => university.properties?.name === parcelUniversity
+			);
+
+			if (university) {
+				const [x, y] = projection(university.geometry.coordinates as [number, number]) ?? [0, 0];
+
+				// Draw university locations.
+				ctx.beginPath();
+				ctx.arc(x, y, 5, 0, 2 * Math.PI);
+				ctx.fillStyle = GRIST_COLORS.EARTH;
+				ctx.fill();
+				ctx.lineWidth = 1;
+				ctx.strokeStyle = GRIST_COLORS.SMOG;
+				ctx.stroke();
+				ctx.closePath();
+
+				// Label backgrounds.
+				ctx.font = 'bold 10px Basis Grotesque Pro';
+				const { width: textWidth } = ctx.measureText(university.properties?.name);
+
+				ctx.shadowColor = 'rgba(60, 56, 48, 0.1)';
+				ctx.shadowOffsetY = 2;
+				ctx.shadowOffsetX = 2;
+				ctx.shadowBlur = 10;
+				ctx.fillStyle = '#ffffff';
+				ctx.fillRect(x + 8, y - 15, textWidth + 8, 16);
+				ctx.strokeStyle = GRIST_COLORS.EARTH;
+				ctx.strokeRect(x + 8, y - 15, textWidth + 8, 16);
+
+				// Labels.
+				ctx.fillStyle = GRIST_COLORS.EARTH;
+				ctx.fillText(university.properties?.name, x + 12, y - 4);
+				ctx.strokeStyle = GRIST_COLORS.SMOG;
+				ctx.lineWidth = 0.1;
+				ctx.strokeText(university.properties?.name, x + 12, y - 4);
+			}
 		});
 	};
 
@@ -72,23 +125,12 @@
 		ctx.lineJoin = 'round';
 		ctx.lineCap = 'round';
 
-		const requests = await Promise.all([fetch('/us.json'), fetch('/parcels.geojson')]);
-		const [usTopoJson, ps] = (await Promise.all(requests.map((res) => res.json()))) as [
-			TopoJSON.Topology,
-			FeatureCollection<Point, ParcelProperties>
-		];
-
-		us = topojson.feature(usTopoJson, usTopoJson.objects.nation) as Feature;
-		parcels = ps.features;
-		domain = [0, d3.max(ps.features, (d) => d.properties.gis_acres ?? 0) ?? 0];
-		scale = d3.scaleLinear(domain, range);
-
-		draw(ps.features.filter(ACTIVITY_CONFIG[$activity].filter), ctx, scale);
+		draw(parcels.features.filter(ACTIVITY_CONFIG[$activity].filter), ctx, scale);
 	});
 
 	$: if (canvas) {
 		const ctx = canvas.getContext('2d')!;
-		draw(parcels.filter(ACTIVITY_CONFIG[$activity].filter), ctx, scale);
+		draw(parcels.features.filter(ACTIVITY_CONFIG[$activity].filter), ctx, scale);
 	}
 </script>
 
