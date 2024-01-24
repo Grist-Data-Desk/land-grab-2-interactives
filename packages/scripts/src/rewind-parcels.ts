214a814e19b08aac365c9160abdf40bf9a99df68
parkerziegler <parkerellisziegler@gmail.com>
2024-01-24 14:42:08 -0800

    feat: Create both point and polygon versions of the STL parcels dataset with reversed winding order.

--- packages/scripts/src/rewind-parcels.ts
+++ packages/scripts/src/rewind-parcels.ts
@@ -2,15 +2,27 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 import * as url from 'node:url';
 
-import type { FeatureCollection, Polygon } from 'geojson';
+import type { FeatureCollection, Feature, Polygon } from 'geojson';
 import type { ParcelProperties } from '@land-grab-2-interactives/types';
 import * as turf from '@turf/turf';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 /**
- * Reverse the winding order of parcel polygons to support D3's clockwise
+ * Reverse the winding order of a parcel polygon to support D3's clockwise
  * winding order. See: https://observablehq.com/@d3/winding-order.
+ */
+const rewind = (
+  parcel: Feature<Polygon, ParcelProperties>
+): Feature<Polygon, ParcelProperties> => {
+  const rewound = turf.rewind(parcel, { reverse: true });
+
+  return turf.feature(rewound.geometry, parcel.properties);
+};
+
+/**
+ * Generate point and polygon versions of the STL dataset with reversed winding
+ * order to support D3 animations.
  */
 const main = async () => {
   const geojson = JSON.parse(
@@ -24,23 +36,33 @@ const main = async () => {
   ) as FeatureCollection<Polygon, ParcelProperties>;
 
   const parcels = turf.featureCollection(
-    geojson.features
-      .map((parcel) => {
-        if (parcel.geometry) {
-          const rewound = turf.rewind(parcel, { reverse: true });
-          const centroid = turf.centroid(rewound);
-
-          return turf.feature(centroid.geometry, parcel.properties);
-        }
-
-        return null;
-      })
-      .filter(Boolean)
+    geojson.features.filter((parcel) => Boolean(parcel.geometry)).map(rewind)
   );
 
+  console.log(
+    'Writing rewound parcels to data/processed/parcels-rewound.geojson.'
+  );
   await fs.writeFile(
     path.join(__dirname, '../data/processed/parcels-rewound.geojson'),
     JSON.stringify(parcels, null, 2)
+  );
+
+  console.log('Extracting centroids of rewound parcels.');
+
+  const centroids = turf.featureCollection(
+    parcels.features.map((parcel) => {
+      const centroid = turf.centroid(parcel);
+
+      return turf.feature(centroid.geometry, parcel.properties);
+    })
+  );
+
+  console.log(
+    'Writing centroids of rewound parcels to data/processed/parcel-centroids-rewound.geojson.'
+  );
+  await fs.writeFile(
+    path.join(__dirname, '../data/processed/parcel-centroids-rewound.geojson'),
+    JSON.stringify(centroids, null, 2)
   );
 };
 
