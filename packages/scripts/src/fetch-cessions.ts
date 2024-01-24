edc265dfe09f555cffe0adeb82d62502271def52
parkerziegler <parkerellisziegler@gmail.com>
2024-01-24 11:14:32 -0800

    feat: Update script for fetching cessions to capture timestamps.

--- packages/scripts/src/fetch-cessions.ts
+++ packages/scripts/src/fetch-cessions.ts
@@ -2,6 +2,12 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 import * as url from 'node:url';
 
+import { FeatureCollection, Polygon } from 'geojson';
+import * as turf from '@turf/turf';
+import _ from 'lodash';
+
+import type { CessionProperties } from '@land-grab-2-interactives/types';
+
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const SERVER_ENDPOINT =
@@ -17,11 +23,58 @@ const main = async () => {
   const endpoint = `${SERVER_ENDPOINT}?where=TRUE%3DTRUE&outFields=*&f=geojson`;
 
   try {
-    const data = await fetch(endpoint).then((res) => res.json());
+    const data = (await fetch(endpoint).then((res) =>
+      res.json()
+    )) as FeatureCollection<Polygon>;
+
+    // Convert dates to a human readable format and pick the subset of fields we need.
+    const features = _.sortBy(
+      data.features.map((feature, i) => {
+        const dates = _.range(1, 6).reduce(
+          (acc, i) => {
+            const date =
+              feature.properties[`TribalCededLandsTable.CessDate${i}`];
+
+            if (!date) {
+              return {
+                ...acc,
+                [`cessionDate${i}`]: null,
+                [`cessionDate${i}Formatted`]: null
+              };
+            }
+
+            const formattedDate = new Date(date).toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric'
+            });
+
+            return {
+              ...acc,
+              [`cessionDate${i}`]: date,
+              [`cessionDate${i}Formatted`]: formattedDate
+            };
+          },
+          {} as Omit<CessionProperties, 'cessionNum'>
+        );
+
+        return {
+          ...feature,
+          properties: {
+            index: i,
+            cessionNumber: feature.properties['TribalCededLandsTable.CessNum'],
+            ...dates
+          }
+        };
+      }),
+      ['cessionDate1']
+    );
+
+    console.log(`Fetched ${features.length} cessions.`);
     console.log('Writing cessions to data/processed/cessions.geojson.');
     await fs.writeFile(
       path.resolve(__dirname, '../data/processed/cessions.geojson'),
-      JSON.stringify(data, null, 2)
+      JSON.stringify(turf.featureCollection(features), null, 2)
     );
   } catch (error) {
     console.error(`Failed to fetch cessions. Error: `, error);
---
+++ packages/types/src/cession.ts
@@ -0,0 +1,13 @@
+export interface CessionProperties {
+  cessionNum: string;
+  cessionDate1: number | null;
+  cessionDate1Formatted: string | null;
+  cessionDate2: number | null;
+  cessionDate2Formatted: string | null;
+  cessionDate3: number | null;
+  cessionDate3Formatted: string | null;
+  cessionDate4: number | null;
+  cessionDate4Formatted: string | null;
+  cessionDate5: number | null;
+  cessionDate5Formatted: string | null;
+}
